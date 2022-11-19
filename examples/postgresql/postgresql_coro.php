bb7661a8d972466dcfb5b06e7cf125f7fcb5cc43
fakharksa <106683667+fakharksa@users.noreply.github.com>
2022-11-19 19:56:31 +0500

    Update postgresql_coro.php
    To update the fact that in latest version of Swoole (as i test on 5.0.1 only and in OpenSwoole), the query() function on "postgresql coroutine object (on $pg)" does not require to pass $conn as first parameter; i think this code (in which $conn is passed as first parameter to query() function) is valid only till some older version of Swoole.
    
    I have not tested same for metaData() function so please test and update this code for metaData() also, if required.

--- examples/postgresql/postgresql_coro.php
+++ examples/postgresql/postgresql_coro.php
@@ -5,7 +5,10 @@ go(function () {
 
     $pg = new Swoole\Coroutine\PostgreSql();
     $conn  = $pg -> connect ("host=127.0.0.1 port=5432 dbname=test user=wuzhenyu password=");
-    $result = $pg->query($conn, 'SELECT * FROM test;');
+    if ($conn === false) {
+       throw new \RuntimeException(sprintf('Failed to connect PostgreSQL server: %s', $conn->error));
+    }
+    $result = $pg->query('SELECT * FROM test;');
     $arr = $pg->fetchAll($result);// the same with affectedRows(),fetchObject(),fetchAssoc(),fetchArray(),fetchRow(),numRows()
     var_dump($arr);
 
